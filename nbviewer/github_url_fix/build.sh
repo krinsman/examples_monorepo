31d293e9a0731faa1331c3cc25d15ecf8fe6e9b6
William Krinsman <krinsman@berkeley.edu>
2019-07-09 14:52:50 -0700

    Refactored some of the NBViewer build setups; massive reduction in code duplication.

--- nbviewer/github_url_fix/build.sh
+++ nbviewer/github_url_fix/build.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
 
-# Make Docker build context the repository root
-export repositoryRoot=../..
-cd $repositoryRoot
-
-# If it wasn't executable before... it is now
-export buildUtils=./useful_functions.sh
-chmod +x $buildUtils
-
-# Check to see whether base image already exists; if it doesn't, then build it
-$buildUtils if_not_base_image_then_build_it
-
-$buildUtils smart_build                        \
-    --file=nbviewer/Dockerfile                 \
-    --build-arg branch=github_url_fix          \
-    --build-arg repository=krinsman            \
-    --tag nbviewer_base                        \
-    .
-
-$buildUtils smart_build                        \
-    --file=nbviewer/github_url_fix/Dockerfile  \
-    --tag jupyter:github_url_fix               \
-    .
-
-# Delete all intermediate images with label autodelete=true
-$buildUtils destroy_intermediates
+chmod +x ../branchBuild.sh
+../branchBuild.sh nbviewerBranchBuild krinsman github_url_fix
--- nbviewer/master/build.sh
+++ nbviewer/master/build.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
 
-# Make Docker build context the repository root
-export repositoryRoot=../..
-cd $repositoryRoot
-
-# If it wasn't executable before... it is now
-export buildUtils=./useful_functions.sh
-chmod +x $buildUtils
-
-# Check to see whether base image already exists; if it doesn't, then build it
-$buildUtils if_not_base_image_then_build_it
-
-$buildUtils smart_build                \
-    --file=nbviewer/Dockerfile         \
-    --build-arg branch=master          \
-    --build-arg repository=jupyter     \
-    --tag nbviewer_base                \
-    .
-
-$buildUtils smart_build                \
-    --file=nbviewer/master/Dockerfile  \
-    --tag jupyter:nbviewer             \
-    .
-
-# Delete all intermediate images with label autodelete=true
-$buildUtils destroy_intermediates
+chmod +x ../branchBuild.sh
+../branchBuild.sh nbviewerBranchBuild jupyter master
