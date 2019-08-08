02f3d4d19ff2cdd2b8e26b4b5e4b219f95a6fbc3
William Krinsman <krinsman@berkeley.edu>
2019-08-08 13:44:23 -0700

    Minor change to reflect update to nbresuse

--- resuse/jupyter_notebook_config.py
+++ resuse/jupyter_notebook_config.py
@@ -1,1 +1,5 @@
-c.ResourceUseDisplay.mem_limit_calculator='jupyterlab_resuse.mem_limit_calculator'
+def mem_limit_calculator(rss, used_mem, total_mem, num_users, cpu_percent):
+    targetPctFraction = 1/num_users
+    return targetPctFraction * total_mem
+
+c.ResourceUseDisplay.mem_limit_calculator=mem_limit_calculator
