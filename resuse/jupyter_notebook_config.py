100a3abbae80958255a04e8b286a42771c3d5528
William Krinsman <krinsman@berkeley.edu>
2019-08-08 17:46:37 -0700

    Updated to reflect updates to my nbresuse fork

--- resuse/jupyter_notebook_config.py
+++ resuse/jupyter_notebook_config.py
@@ -2,4 +2,4 @@ def mem_limit_calculator(rss, used_mem, total_mem, num_users, cpu_percent):
     targetPctFraction = 1/num_users
     return targetPctFraction * total_mem
 
-c.ResourceUseDisplay.mem_limit_calculator=mem_limit_calculator
+c.ResourceUseDisplay.mem_limit_calculator = mem_limit_calculator
